e31f99afa7fed7c5dac06e8a20dfcd9342531f83
Marcello Joaquim da Silva <marcellojoaquim1@hotmail.com>
2022-11-16 23:41:57 -0300

    criação da classe Professor e da interface professorRepository

--- projeto-golearn/go.learn/src/main/java/com/go/learn/repository/ProfessorRepository.java
+++ projeto-golearn/go.learn/src/main/java/com/go/learn/repository/ProfessorRepository.java
@@ -6,6 +6,4 @@ import org.springframework.stereotype.Repository;
 import com.go.learn.model.Professor;
 
 @Repository
-public interface ProfessorRepository extends JpaRepository<Professor, Long> {
-
-}
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {}
