0ec5d8f7a7e6b75a7ca5a543b1f76c6af4bf3889
Flaviox625 <92454976+Flaviox625@users.noreply.github.com>
2022-11-25 20:59:17 -0300

    att2

---
+++ projeto-golearn/go.learn/src/main/java/com/go/learn/dto/GoLearnDiaAulaDto.java
@@ -0,0 +1,23 @@
+package com.go.learn.dto;
+
+public class GoLearnDiaAulaDto {
+
+    private int curso_IdCurso;
+    private int dataAula;
+
+    public int getCurso_IdCurso() {
+        return curso_IdCurso;
+    }
+    public void setCurso_IdCurso(int curso_IdCurso) {
+        this.curso_IdCurso = curso_IdCurso;
+    }
+    public int getDataAula() {
+        return dataAula;
+    }
+    public void setDataAula(int dataAula) {
+        this.dataAula = dataAula;
+    }
+
+    
+    
+}
