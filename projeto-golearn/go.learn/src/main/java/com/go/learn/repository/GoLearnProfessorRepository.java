939f48bb2d3372d51fee32e9fdda2d24659cf5ee
José Vanildo <103756009+jvanildo@users.noreply.github.com>
2022-11-25 20:46:17 -0300

    atualizacao do codigo

--- projeto-golearn/go.learn/src/main/java/com/go/learn/repository/GoLearnProfessorRepository.java
+++ projeto-golearn/go.learn/src/main/java/com/go/learn/repository/GoLearnProfessorRepository.java
@@ -6,4 +6,12 @@ import org.springframework.stereotype.Repository;
 import com.go.learn.model.GoLearnProfessorModel;
 
 @Repository
+<<<<<<< Updated upstream:projeto-golearn/go.learn/src/main/java/com/go/learn/repository/GoLearnProfessorRepository.java
 public interface GoLearnProfessorRepository extends JpaRepository<GoLearnProfessorModel, Long> {}
+=======
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+
+
+    
+}
+>>>>>>> Stashed changes:projeto-golearn/go.learn/src/main/java/com/go/learn/repository/ProfessorRepository.java
